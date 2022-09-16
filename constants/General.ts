e2e2590d5529ea73766607f43ac6ae48007980b7
Amit Yadav <amit.yadav@go-mmt.com>
2022-09-16 16:18:43 +0530

    Change build version

--- constants/General.ts
+++ constants/General.ts
@@ -1,2 +1,2 @@
-export const topicIP = "http://localhost:8000";
-// export const topicIP = "http://35.154.90.65:8000";
+// export const topicIP = "http://localhost:8000";
+export const topicIP = "http://35.154.90.65:8000";
